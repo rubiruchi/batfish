5332f1e70d8f00506322ceb1e8d57aeb28d289e0
Victor Heorhiadi <victor@intentionet.com>
2018-12-26 12:23:50 -0800

    Move BGP topology into the topology provider

--- projects/batfish-common-protocol/src/main/java/org/batfish/common/topology/IpOwners.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/common/topology/IpOwners.java
@@ -4,10 +4,13 @@ import static org.batfish.common.topology.TopologyUtil.computeIpInterfaceOwners;
 import static org.batfish.common.topology.TopologyUtil.computeNodeInterfaces;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.stream.Collectors;
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
 import org.batfish.common.util.CommonUtil;
 import org.batfish.datamodel.AclIpSpace;
 import org.batfish.datamodel.Configuration;
@@ -16,25 +19,32 @@ import org.batfish.datamodel.Ip;
 import org.batfish.datamodel.IpSpace;
 
 /** A utility class for working with IPs owned by network devices. */
+@ParametersAreNonnullByDefault
 public final class IpOwners {
 
   /** Mapping from a hostname to a set of all (including inactive) interfaces that node owns */
-  private final Map<String, Set<Interface>> _allInterfaces;
+  @Nonnull private final Map<String, Set<Interface>> _allInterfaces;
 
   /**
    * Mapping from a IP to hostname to set of interfaces that own that IP (including inactive
    * interfaces)
    */
-  private final Map<Ip, Map<String, Set<String>>> _allDeviceOwnedIps;
+  @Nonnull private final Map<Ip, Map<String, Set<String>>> _allDeviceOwnedIps;
 
   /**
    * Mapping from a IP to hostname to set of interfaces that own that IP (for active interfaces
    * only)
    */
-  private final Map<Ip, Map<String, Set<String>>> _activeDeviceOwnedIps;
+  @Nonnull private final Map<Ip, Map<String, Set<String>>> _activeDeviceOwnedIps;
 
   /** Mapping from hostname to interface name to IpSpace owned by that interface */
-  private final Map<String, Map<String, IpSpace>> _hostToInterfaceToIpSpace;
+  @Nonnull private final Map<String, Map<String, IpSpace>> _hostToInterfaceToIpSpace;
+
+  /** Mapping from IP to a set of hostnames that own that IP, for all IPs. */
+  @Nonnull private final Map<Ip, Set<String>> _allIpToOwnerHostnames;
+
+  /** Mapping from IP to a set of hostnames that own that IP, for active IPs only. */
+  @Nonnull private final Map<Ip, Set<String>> _activeIpToOwnerHostnames;
 
   public IpOwners(Map<String, Configuration> configurations) {
     _allInterfaces = ImmutableMap.copyOf(computeNodeInterfaces(configurations));
@@ -42,6 +52,8 @@ public final class IpOwners {
     _activeDeviceOwnedIps = ImmutableMap.copyOf(computeIpInterfaceOwners(_allInterfaces, true));
     _hostToInterfaceToIpSpace =
         ImmutableMap.copyOf(computeInterfaceOwnedIpSpaces(_activeDeviceOwnedIps));
+    _allIpToOwnerHostnames = removeInterfaceInformation(_allDeviceOwnedIps);
+    _activeIpToOwnerHostnames = removeInterfaceInformation(_activeDeviceOwnedIps);
   }
 
   /**
@@ -67,6 +79,20 @@ public final class IpOwners {
   }
 
   /**
+   * Remove interface information of IP owners map and reduce the map to only contain mappings from
+   * IP to set of hostnames
+   */
+  private static Map<Ip, Set<String>> removeInterfaceInformation(
+      Map<Ip, Map<String, Set<String>>> allDeviceOwnedIps) {
+    return CommonUtil.toImmutableMap(
+        allDeviceOwnedIps,
+        Entry::getKey, /* Ip */
+        ipInterfaceOwnersEntry ->
+            /* project away interfaces */
+            ipInterfaceOwnersEntry.getValue().keySet());
+  }
+
+  /**
    * Mapping from a IP to hostname to set of interfaces that own that IP (for active interfaces
    * only)
    */
@@ -88,5 +114,24 @@ public final class IpOwners {
    */
   public Map<String, Map<String, IpSpace>> getInterfaceOwnedIpSpaces() {
     return _hostToInterfaceToIpSpace;
+  }
+
+  /**
+   * Return the set of hostnames that own a given IP address. If no such device exists, an empty set
+   * is returned.
+   *
+   * <p><b>Note:</b> set of IPs analyzed includes inactive interfaces.
+   */
+  @Nonnull
+  public Set<String> getOwnerHostnames(Ip ip) {
+    return _allIpToOwnerHostnames.getOrDefault(ip, ImmutableSet.of());
+  }
+
+  /**
+   * Return the mapping from all active IPs in the network to the set of hostnames that own that IP.
+   */
+  @Nonnull
+  public Map<Ip, Set<String>> getActiveIpToOwnerHostnames() {
+    return _activeIpToOwnerHostnames;
   }
 }
--- projects/batfish/src/main/java/org/batfish/topology/TopologyProviderImpl.java
+++ projects/batfish/src/main/java/org/batfish/topology/TopologyProviderImpl.java
@@ -2,25 +2,31 @@ package org.batfish.topology;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.common.graph.ValueGraph;
 import java.util.concurrent.ExecutionException;
 import javax.annotation.ParametersAreNonnullByDefault;
 import org.batfish.common.NetworkSnapshot;
 import org.batfish.common.plugin.IBatfish;
 import org.batfish.common.topology.IpOwners;
 import org.batfish.common.topology.TopologyProvider;
+import org.batfish.datamodel.BgpPeerConfigId;
+import org.batfish.datamodel.BgpSessionProperties;
+import org.batfish.datamodel.bgp.BgpTopologyUtils;
 
-/** */
 @ParametersAreNonnullByDefault
 public final class TopologyProviderImpl implements TopologyProvider {
   private static final int MAX_CACHED_SNAPSHOTS = 3;
 
   private final IBatfish _batfish;
   private final Cache<NetworkSnapshot, IpOwners> _ipOwners;
+  private final Cache<NetworkSnapshot, ValueGraph<BgpPeerConfigId, BgpSessionProperties>>
+      _inferredBgpTopology;
 
   /** Create a new topology provider for a given instance of {@link IBatfish} */
   public TopologyProviderImpl(IBatfish batfish) {
     _batfish = batfish;
     _ipOwners = CacheBuilder.newBuilder().maximumSize(MAX_CACHED_SNAPSHOTS).build();
+    _inferredBgpTopology = CacheBuilder.newBuilder().maximumSize(MAX_CACHED_SNAPSHOTS).build();
   }
 
   /** Return the {@link IpOwners} for a given snapshot. */
@@ -30,6 +36,30 @@ public final class TopologyProviderImpl implements TopologyProvider {
       return _ipOwners.get(snapshot, () -> new IpOwners(_batfish.loadConfigurations(snapshot)));
     } catch (ExecutionException e) {
       return new IpOwners(_batfish.loadConfigurations(snapshot));
+    }
+  }
+
+  /**
+   * Return the BGP topology inferred by Batfish based on configured BGP peers.
+   *
+   * <p>This topology performs basic peer compatibility checks but is unaware of dataplane
+   * reachability for actual session establishment.
+   */
+  public ValueGraph<BgpPeerConfigId, BgpSessionProperties> getInferredBgpTopology(
+      NetworkSnapshot snapshot) {
+    try {
+      return _inferredBgpTopology.get(
+          snapshot,
+          () ->
+              BgpTopologyUtils.initBgpTopology(
+                  _batfish.loadConfigurations(snapshot),
+                  getIpOwners(snapshot).getActiveIpToOwnerHostnames(),
+                  false));
+    } catch (ExecutionException e) {
+      return BgpTopologyUtils.initBgpTopology(
+          _batfish.loadConfigurations(snapshot),
+          getIpOwners(snapshot).getActiveIpToOwnerHostnames(),
+          false);
     }
   }
 }
